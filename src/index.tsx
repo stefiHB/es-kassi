5d0f468d41cb02f0ffee751eb68b4a22a3128223
Stephanie Kassinopoulou <kassinopoulou.stephanie@ucy.ac.cy>
2024-04-30 12:53:42 +0300

    refa: slight changes with page ico

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,7 @@ ReactDOM.render(
             <App />
         </React.StrictMode>
     </BrowserRouter>
-,
-  document.getElementById('root')
-);
+, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
